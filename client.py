102bcb776e598572fa300de11ff001d8587f0a64
teckniec <1anthony1@gmail.com>
2020-05-11 18:37:14 -0700

    fully functional

--- client.py
+++ client.py
@@ -5,6 +5,14 @@ def get_selected_row(event):
     global selected_tuple
     index=list1.curselection()[0]
     selected_tuple=list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4]) 
 def view_command():
     list1.delete(0,END)
     for row in server.view():
@@ -19,9 +27,11 @@ def add_command():
     list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
 def delete_command():
     server.delete(selected_tuple[0])
+def upadate_command():
+    server.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
 
 window=Tk()
-
+window.wm_title("Book Manager")
 l1=Label(window,text="Title")
 l1.grid(row=0,column=0)
 
@@ -39,16 +49,16 @@ e1=Entry(window, textvariable=title_text)
 e1.grid(row=0,column=1)
 
 author_text=StringVar()
-e1=Entry(window, textvariable=author_text)
-e1.grid(row=0,column=3)
+e2=Entry(window, textvariable=author_text)
+e2.grid(row=0,column=3)
 
 year_text=StringVar()
-e1=Entry(window, textvariable=year_text)
-e1.grid(row=1,column=1)
+e3=Entry(window, textvariable=year_text)
+e3.grid(row=1,column=1)
 
 isbn_text=StringVar()
-e1=Entry(window, textvariable=isbn_text)
-e1.grid(row=1,column=3)
+e4=Entry(window, textvariable=isbn_text)
+e4.grid(row=1,column=3)
 
 list1=Listbox(window, height=6,width=35)
 list1.grid(row=2,column=0,rowspan=6,columnspan=2)
@@ -70,13 +80,13 @@ b2.grid(row=3,column=3)
 b3=Button(window,text="Add",width=12,command=add_command)
 b3.grid(row=4,column=3)
 
-b4=Button(window,text="Update",width=12)
+b4=Button(window,text="Update",width=12,command=upadate_command)
 b4.grid(row=5,column=3)
 
 b5=Button(window,text="Delete",width=12,command=delete_command)
 b5.grid(row=6,column=3)
 
-b6=Button(window,text="Close",width=12)
+b6=Button(window,text="Close",width=12,command=window.destroy)
 b6.grid(row=7,column=3)
 
 window.mainloop()
